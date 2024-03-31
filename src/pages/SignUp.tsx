03062c9728be68729f08075c897391e52a004933
karan-gander <meghawalkaran@gmail.com>
2024-03-31 10:35:12 +0530

    form validtaion has been started from tommarrow

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,64 +1,94 @@
 // import { Link} from "react-router-dom"
 import { ArrowRight } from "lucide-react";
-import {CustomApiHandlePost} from "../controllers/CustomApiHandlePost"
-import React, {useState} from "react"
+import { CustomApiHandlePost } from "../controllers/CustomApiHandlePost";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form"
 
 export default function SignUpThree() {
+  const [response, setResponse] = useState({ message: "", statusCode: false,data:{},success:false });
+  
+  const [formdata, setFormData] = useState({
+    email: "",
+    fullname: "",
+    sponsorId: "",
+    password: "",
+    transactionPin: "",
+  });
 
-  const [error,setError] = useState(false)
-  // const [loading,setLoading] = useState(false)
-  const [isLogin,setIslogin]  = useState(false)
+  // form validation hooks
+  const  {register,handleSubmit,formState:{errors}} = useForm()
 
-  const [formdata,setFormData] = useState({
-    email:"",
-    fullname:"",
-    sponsorId:"",
-    password:"",
-    transactionPin:""
-  })
 
-  const handleFormData = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        
-    setFormData((preValue)=>{
-      return {...preValue,[e.target.name]:[e.target.value]}
-    })
 
-  }
-   
-  const handleSubmit = (event:React.FocusEvent<HTMLFormElement>)=>{
-    event.preventDefault()
+  const handleFormData = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((preValue) => {
+      return { ...preValue, [e.target.name]: [e.target.value] };
+    });
+  };
+
+
+  const onSubmit = () => {
+    // event.preventDefault();
     const FormData = {
-      email:formdata.email[0],
-      fullname:formdata.fullname[0],
-      sponsorId:formdata.sponsorId[0],
-      password:formdata.password[0],
-      transactionPin:formdata.transactionPin[0],
-    }
-    console.log(FormData.sponsorId)
-    
-    const userSignedIn = CustomApiHandlePost("/api/v1/users/register",FormData)
-    
-    userSignedIn.then((response)=>{
-      console.log(response.data);
-      console.log(response.response.data);
-     
-      
-    }).catch((err)=>{
-      console.log("cath",err)
-      console.log(err.response);
-      
-    })
-  }
+      email: formdata.email[0],
+      fullname: formdata.fullname[0],
+      sponsorId: formdata.sponsorId[0],
+      password: formdata.password[0],
+      transactionPin: formdata.transactionPin[0],
+    };
+    console.log(FormData.sponsorId);
+
+    // calling custom hook for api calling
+    const userSignedIn = CustomApiHandlePost(
+      "/api/v1/users/register",
+      FormData
+    );
+
+
+
+
+    userSignedIn
+      .then((response) => {
+       
+        // const { data: message="", status } = response.response;
+        if(response.response===undefined || !response){
+        const {data,message,statusCode,success} =  response
+        if(response){
+          setResponse({
+            data:data,
+            message:message,
+            statusCode:statusCode,
+            success:success
+          })
+        }
+        return;
+      }
+
 
       
-      // console.log(formdata)
+        const {data:Message,status} = response.response 
+        console.log(Message,status, " here my data")
 
-   
+        const startIndex = Message.indexOf("Error: ") + "Error: ".length;
+        const endIndex = Message.indexOf("<br>");
+        const errorMessage = Message.slice(startIndex, endIndex);
+        console.log(errorMessage)
+
+        
+        
+      })
+      .catch((err) => {
+        console.log("cath", err);
+        console.log(err.response);
+      });
+  };
+
+  // console.log(formdata)
+
   return (
     <section>
       <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-          
           <h2 className="text-center text-2xl font-bold leading-tight text-black">
             Sign up to create account
           </h2>
@@ -72,17 +102,21 @@ export default function SignUpThree() {
               Sign In
             </a>
           </p>
-          <form action="#" method="POST" className="mt-8" onSubmit={handleSubmit}>
+          <form
+            action="#"
+            method="POST"
+            className="mt-8"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className="space-y-5">
-              
-            <div>
+              <div>
                 <div className="flex items-center justify-between">
                   <label
                     htmlFor="email"
                     className="text-base font-medium text-gray-900"
                   >
                     {" "}
-                    Email {" "}
+                    Email{" "}
                   </label>
                 </div>
                 <div className="mt-2">
@@ -91,10 +125,12 @@ export default function SignUpThree() {
                     type="email"
                     placeholder="Email"
                     id="email"
+                    {...register("email",{required:"Email is required"})}
                     name="email"
                     onChange={handleFormData}
                   ></input>
-                </div>
+                  {errors.email && <span>email is required</span>}
+                </div>  
               </div>
 
               <div>
@@ -179,7 +215,6 @@ export default function SignUpThree() {
                     placeholder="TransactionPin"
                     id="transactionPin"
                     name="transactionPin"
-              
                     onChange={handleFormData}
                   ></input>
                 </div>
@@ -194,40 +229,7 @@ export default function SignUpThree() {
               </div>
             </div>
           </form>
-          {/* <div className="mt-3 space-y-3">
-            <button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-rose-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                </svg>
-              </span>
-              Sign up with Google
-            </button>
-            <button
-              type="button"
-              className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-            >
-              <span className="mr-2 inline-block">
-                <svg
-                  className="h-6 w-6 text-[#2563EB]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                </svg>
-              </span>
-              Sign up with Facebook
-            </button>
-          </div> */}
+          
         </div>
       </div>
     </section>
